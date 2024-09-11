18129cec646f0aec79a207d02c086c31f6c071b4
anupeesay10 <141187396+anupeesay10@users.noreply.github.com>
2024-09-11 11:48:35 -0400

    Add files via upload

---
+++ pythagorean.py
@@ -0,0 +1,15 @@
+import math
+
+
+if __name__ == "__main__":
+
+    side1 = input("Please enter the length of side one: ")
+    side2 = input("Please enter the length of side two: ")
+
+    side1 = float(side1)
+    side2 = float(side2)
+
+    path = math.sqrt((side1 ** 2) + (side2 ** 2))
+
+    print("The hypotenuse is: %.2f" %(path))
+
